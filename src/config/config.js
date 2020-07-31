ce3390be374de5e2783c889e0a311ea641377d6e
Pavel Saikov <pavsaikov@gmail.com>
2020-07-31 19:23:41 +0300

    init: start task songbird

---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+export const config = {
+  BIRDS_VOICE_API_KEY,
+  BIRDS_IMAGE_API_KEY,
+};
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+require('dotenv').config();
+
+module.exports = (envOptions) => {
+  const mode = envOptions && envOptions.production ? 'production' : 'development';
+
+  return {
+    entry: path.resolve(__dirname, './src/index.jsx'),
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, './dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          use: ['babel-loader', 'eslint-loader'],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|gif|woff|ttf)$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 3000,
+      historyApiFallback: true,
+      watchContentBase: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html',
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+    ],
+    devtool: 'source-map',
+    mode: 'development',
+  };
+};
